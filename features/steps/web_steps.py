24f935e146eb0cbd2a5ae874f701436ba32e0973
kaelynj <43348706+kaelynj@users.noreply.github.com>
2024-06-09 21:21:31 -0400

    Fix final BDD test

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -109,7 +109,7 @@ def step_impl(context, element_name):
 def step_impl(context, button):
     button_id = button.lower() + '-btn'
     context.driver.find_element_by_id(button_id).click()
-  
+
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
--- service/routes.py
+++ service/routes.py
@@ -101,31 +101,35 @@ def create_products():
 # PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
 #
 @app.route("/products", methods=["GET"])
-def list_produces():
-    """ Lists all products """
-    app.logger.info("Listing all products")
+def list_products():
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
 
     products = []
     name = request.args.get("name")
     category = request.args.get("category")
-    availability = request.args.get("availability")
+    available = request.args.get("available")
+
     if name:
         app.logger.info("Find by name: %s", name)
         products = Product.find_by_name(name)
     elif category:
         app.logger.info("Find by category: %s", category)
+        # create enum from string
         category_value = getattr(Category, category.upper())
         products = Product.find_by_category(category_value)
-    elif availability:
-        app.logger.info("Find by availability: %s", availability)
-        products = Product.find_by_availability(availability)
+    elif available:
+        app.logger.info("Find by available: %s", available)
+        # create bool from string
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
     else:
         app.logger.info("Find all")
         products = Product.all()
 
-    product_list = [product.serialize() for product in products]
-    app.logger.info(f"Returning {len(product_list)} products")
-    return product_list, status.HTTP_200_OK
+    results = [product.serialize() for product in products]
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
 
 ######################################################################
 # R E A D   A   P R O D U C T
